f43454edec42ed98c19d2ebec60a3926c4034af0
Caian R. Ertl <hi@caian.org>
2021-04-25 05:49:32 -0300

    feat: package main logic

---
+++ vrelease-bin/__init__.py
@@ -0,0 +1,45 @@
+# standard
+import sys
+import platform
+from os import system
+from os.path import abspath
+from os.path import dirname
+from os.path import join
+
+
+def get_platform_bin():
+    plt = lambda s: 'vrelease-' + s
+    system = platform.system()
+
+    if system == 'Linux':
+        return plt('linux')
+
+    if system == 'Darwin':
+        return plt('macos')
+
+    if system == 'Windows':
+        return plt('windows.exe')
+
+    raise RuntimeError(f'unsupported platform {system}')
+
+
+def main():
+    try:
+        arch = platform.machine()
+        if arch != 'x86_64':
+            raise RuntimeError(f'unsupported architecture {arch}')
+
+        file = get_platform_bin()
+        bin_path = abspath(join(dirname(__file__), 'bin', file))
+
+        cli_input = sys.argv[1:]
+        cmd = '{} {}'.format(bin_path, ' '.join(cli_input)).strip()
+        system(cmd)
+
+    except Exception as err:
+        print(str(err))
+        sys.exit(2)
+
+
+if __name__ == '__main__':
+    main()
